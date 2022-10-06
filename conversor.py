3649909eab969a3fa5c7c187140358832de7f337
Barbara Mercez <92188542+BarbaraMercez@users.noreply.github.com>
2022-10-06 15:54:30 +0000

    Initial commit

---
+++ conversor.py
@@ -0,0 +1,4 @@
+def conversor():
+  valor_cm = float(input('Insira o valor em centímetros a ser convertido para polegadas: '))
+  valor_pol = valor_cm / 2.54
+  print(f'O valor {valor_cm} em centímetros corresponde a {valor_pol} valor em polegadas.')
